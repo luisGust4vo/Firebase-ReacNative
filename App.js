9e6bcb1b174df76abb6f817137c7cd40d8a61f56
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2023-03-01 22:24:18 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text} from 'react-native';
+import firebase from './src/firebaseConnection';
+
+console.disableYellowBox=true;
+
+export default function App(){
+  const [nome, setNome] = useState('Carregando...');
+  const [idade, setIdade] = useState('');
+
+  useEffect(()=> {
+
+    async function dados(){
+
+      //Olheiro da nossa database
+      await firebase.database().ref('usuarios/2').on('value', (snapshot) => {
+        setNome(snapshot.val().nome);
+        setIdade(snapshot.val().idade);
+      });
+
+      // await firebase.database().ref('nome').once('value', (snapshot)=> {
+      //   setNome(snapshot.val());
+      // });
+
+    }
+
+    dados();
+
+
+  }, []);
+
+  return(
+    <View style={{marginTop: 25}}>
+      <Text style={{fontSize: 25}}>Olá {nome}</Text>
+      <Text style={{fontSize: 25}}>Idade {idade}</Text>
+    </View>
+  );
+}
